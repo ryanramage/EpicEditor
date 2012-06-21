6e3ae1b5258929bae20dc27b1e51c4d66886b4ee
Ryan Ramage <ryan.ramage@gmail.com>
2012-06-21 16:41:34 -0600

    Update base path to work with Jam location

--- src/editor.js
+++ src/editor.js
@@ -278,7 +278,7 @@ define('EpicEditor',['marked'], function(marked){
       , _defaultFileSchema
       , _defaultFile
       , defaults = { container: 'epiceditor'
-        , basePath: 'epiceditor'
+        , basePath: 'jam/EpicEditor/epiceditor'
         , localStorageName: 'epiceditor'
         , file: { name: null
         , defaultContent: ''
